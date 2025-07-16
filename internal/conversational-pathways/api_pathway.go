1c879133d9e6e076c9d9640155c368801f9b981d
James Hiester <james.hiester@fake.com>
2025-07-15 23:52:20 -0400

    updating update pathway method

--- internal/conversational-pathways/api_pathway.go
+++ internal/conversational-pathways/api_pathway.go
@@ -76,7 +76,7 @@ func (client *client) UpdatePathway(ctx context.Context, pathwayID string, pathw
 	apiUrl.RawQuery = values.Encode()
 
 	updateResponse := updatePathwayResponseDto{}
-	_, err = client.Api.Execute(ctx, nil, "PUT", apiUrl.String(), nil, pathwayToUpdate, []int{http.StatusOK}, &updateResponse)
+	_, err = client.Api.Execute(ctx, nil, "POST", apiUrl.String(), nil, pathwayToUpdate, []int{http.StatusOK}, &updateResponse)
 	if err != nil {
 		return nil, fmt.Errorf("failed to update connection: %w", err)
 	}
